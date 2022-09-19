1a01b1c6fd4ba13bebb9058f869ac8df46f2fb13
khalid ait bellahs <kaitbellahs@gmail.com>
2022-09-19 12:22:17 -0300

    Update

--- src/job.ts
+++ src/job.ts
@@ -1,82 +1,86 @@
-import { Utils, DBModels, Types, Domain, GateWays } from '@ikomida/shared-backend'
-import {
-    createRequire
-} from "module";
-const require = createRequire(
-    import.meta.url)
-let {
-    name
-} = require("../package.json")
+import { Utils, DBModels, Types, Domain, GateWays } from '@ikomida/shared-backend';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+let { name } = require('../package.json');
 name = name
-    .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-    .replace(/^\w/, (m: string) => m.toUpperCase())
-    .replace(/-\w/g, (m: string[]) => m[1].toUpperCase())
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+  .replace(/^\w/, (m: string) => m.toUpperCase())
+  .replace(/-\w/g, (m: string[]) => m[1].toUpperCase());
 
 class ContractsCheckJob {
-    logger
-    asaasGateway?: GateWays.Asaas
+  logger;
+  asaasGateway?: GateWays.Asaas;
 
-    constructor() {
-        this.logger = Utils.Logger.getInstance(name)
+  constructor() {
+    this.logger = Utils.Logger.getInstance(name);
+    DBModels.SettingModel.findOne({
+      where: {
+        name: 'sandBox',
+      },
+    })
+      .then((setting) => {
         DBModels.SettingModel.findOne({
-            where: {
-                name: 'sandBox',
-            }
-        }).then(setting => {
-            DBModels.SettingModel.findOne({
-                where: {
-                    name: setting?.value === '1' ? 'asaasAccessTokenSandBox' : 'asaasAccessToken',
-                }
-            }).then(asaasAccessToken => {
-                this.asaasGateway = new GateWays.Asaas(this.logger, asaasAccessToken?.value ?? '', setting?.value === '1')
-            }).catch(exception => {
-                console.error(exception)
-                process.exit(1)
-            })
-        }).catch(exception => {
-            console.error(exception)
-            process.exit(1)
+          where: {
+            name: setting?.value === '1' ? 'asaasAccessTokenSandBox' : 'asaasAccessToken',
+          },
         })
-    }
+          .then((asaasAccessToken) => {
+            this.asaasGateway = new GateWays.Asaas(this.logger, asaasAccessToken?.value ?? '', setting?.value === '1');
+          })
+          .catch((exception) => {
+            console.error(exception);
+            process.exit(1);
+          });
+      })
+      .catch((exception) => {
+        console.error(exception);
+        process.exit(1);
+      });
+  }
 
-    async run() {
-        try {
-            const contracts = await DBModels.ContractModel.findAll({
-                order: [
-                    ['createdAt', 'DESC']
-                ],
-                include: {
-                    model: DBModels.ContractPaymentSignatureModel,
-                    required: true,
-                    where: {
-                        status: {
-                            [Domain.SqlDB.Op.notIn]: [Types.Types.TAsaasSignatureStatus.CANCELED]
-                        }
-                    }
-                }
-            })
-            for (const contract of contracts) {
-                const contractPaymentSignature = contract?.contractPaymentSignature as DBModels.ContractPaymentSignatureModel
-                if (!contractPaymentSignature && !((contractPaymentSignature as DBModels.ContractPaymentSignatureModel).subscriptionID)) {
-                    continue
-                }
-                const subscriptionResponse = await this.asaasGateway?.getSubscription(contractPaymentSignature?.subscriptionID ?? '')
-                if (!subscriptionResponse?.success) {
-                    continue
-                }
-                const subscription = subscriptionResponse.data
-                const paymentStatus = Types.Types.TAsaasSignatureStatus.valueOf(subscription?.status)
-                if (paymentStatus && contractPaymentSignature) {
-                    contractPaymentSignature.status = subscription?.deleted ? Types.Types.TAsaasSignatureStatus.CANCELED : paymentStatus
-                    contractPaymentSignature.cycle = subscription?.cycle
-                    contractPaymentSignature.value = subscription?.value
-                    await contractPaymentSignature.save()
-                }
-            }
-        } catch (exception) {
-            this.logger.error(exception)
+  async run() {
+    try {
+      const contracts = await DBModels.ContractModel.findAll({
+        order: [['createdAt', 'DESC']],
+        include: {
+          model: DBModels.ContractPaymentSignatureModel,
+          required: true,
+          where: {
+            status: {
+              [Domain.SqlDB.Op.notIn]: [Types.Types.TAsaasSignatureStatus.CANCELED],
+            },
+          },
+        },
+      });
+      for (const contract of contracts) {
+        const contractPaymentSignature = contract.contractPaymentSignature;
+        if (
+          !(contractPaymentSignature instanceof DBModels.ContractPaymentSignatureModel) ||
+          !contractPaymentSignature?.subscriptionID
+        ) {
+          this.logger.error('A assinatura ou id nao foi localizado!');
+          continue;
         }
+        const subscriptionResponse = await this.asaasGateway?.getSubscription(contractPaymentSignature.subscriptionID);
+        if (!subscriptionResponse?.success) {
+          this.logger.error(`Nao foi possivel obter a assinatura: ${contractPaymentSignature.subscriptionID}!`);
+          continue;
+        }
+        const subscription = subscriptionResponse.data;
+        const paymentStatus = subscription?.status;
+        if (paymentStatus) {
+          contractPaymentSignature.status = subscription?.deleted
+            ? Types.Types.TAsaasSignatureStatus.CANCELED
+            : paymentStatus;
+          contractPaymentSignature.cycle = subscription?.cycle;
+          contractPaymentSignature.value = subscription?.value;
+          await contractPaymentSignature.save();
+        }
+      }
+    } catch (exception) {
+      this.logger.error(exception);
     }
+  }
 }
 
-await (new ContractsCheckJob).run()
+await new ContractsCheckJob().run();
